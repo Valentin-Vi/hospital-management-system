2ae65c5c649c391d7bfb19819ee4b6edb6d9d776
Valentin <valentin.villa@protonmail.com>
2025-09-25 14:33:55 -0300

    Removed depricated use of useAuthRefresh() hook within frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,8 @@ import { RouterProvider } from 'react-router'
 import { AppRouter } from '@router'
 import { AuthProvider } from 'security/AuthProvider'
 import { BackendProvider } from 'hooks/BackendProvider'
-import useAuthRefresh from 'hooks/useAuthRefresh'
 
 function App() {
-  useAuthRefresh()
-
   return (
     <AuthProvider>
       <BackendProvider>
