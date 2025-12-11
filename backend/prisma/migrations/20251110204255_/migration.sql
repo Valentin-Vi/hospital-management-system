c54560e554d1c2085b2c47179dbd972b758be731
Valen <valentin.villa@protonmail.com>
2025-12-11 02:34:46 -0300

    Added repositories

--- backend/prisma/migrations/20251110204255_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `minimun_quantity` on the `inventory` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "inventory" DROP COLUMN "minimun_quantity",
-ADD COLUMN     "minimum_quantity" INTEGER NOT NULL DEFAULT 0;
